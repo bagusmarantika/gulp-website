d5fa4dad57f5b46882c91acb7abb94b190950995
@bagusbregh <bagusbregh@gmail.com>
2021-04-27 06:55:11 +0700

    edit

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,48 @@
 
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    cleanCSS = require('gulp-clean-css'),
-    rename = require('gulp-rename'),
-    purgecss = require('gulp-purgecss'),
-    browserSync = require('browser-sync').create();
+sass = require('gulp-sass'),
+autoprefixer = require('gulp-autoprefixer'),
+cleanCSS = require('gulp-clean-css'),
+rename = require('gulp-rename'),
+purgecss = require('gulp-purgecss'),
+browserSync = require('browser-sync').create();
 var deploy = require('gulp-gh-pages');
 
-    function css() {
-        return gulp.src('./src/assets/css/main.scss')
-          .pipe(sass().on('error', sass.logError))
-          .pipe(autoprefixer())
-          .pipe(cleanCSS({compatibility: 'ie8'}))
-          .pipe(rename(function(path) {
-            path.extname = ".min.css";
-          }))
-          .pipe(
-            purgecss({
-              content: ['public/**/*.html']
-            })
-          )
-          .pipe(gulp.dest('./public/assets/css/'))      
-          .pipe(browserSync.stream());
-      }
-
-      function html() {
-        return gulp.src('./src/**/*.html')
-          .pipe(gulp.dest('./public/'))
-          .pipe(browserSync.stream());
-      }
-
-      function serve() {
-        browserSync.init({
-          server: {
-            baseDir: './public'
-          }
+function css() {
+    return gulp.src('./src/assets/css/main.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer())
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(rename(function(path) {
+        path.extname = ".min.css";
+      }))
+      .pipe(
+        purgecss({
+          content: ['public/**/*.html']
         })
-      }
+      )
+      .pipe(gulp.dest('./public/assets/css/'))      
+      .pipe(browserSync.stream());
+  }
 
-      function icons() {
-        return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
-        .pipe(gulp.dest('.public/assets/webfonts/'));
+  function html() {
+    return gulp.src('./src/**/*.html')
+      .pipe(gulp.dest('./public/'))
+      .pipe(browserSync.stream());
+  }
+
+  function serve() {
+    browserSync.init({
+      server: {
+        baseDir: './public'
       }
+    })
+  }
+
+  function icons() {
+    return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
+    .pipe(gulp.dest('.public/assets/webfonts/'));
+  }
 
 gulp.watch('./src/assets/css/**/*.scss', css);
 gulp.watch('./src/*.html', html).on('change', browserSync.reload);
@@ -50,7 +50,15 @@ gulp.watch('./src/*.html', html).on('change', browserSync.reload);
 exports.default = gulp.parallel(html, css, icons, serve);
 
 
+// gulp.task('deploy', function () {
+//   return gulp.src("./public/**/*")
+//     .pipe(deploy({ 
+//       remoteUrl: "https://github.com/bagusmarantika/gulp-website.git",
+//       branch: "master"
+//     }))
+// });
+
 gulp.task('deploy', function () {
-    return gulp.src("./public/**/*")
-      .pipe(deploy())
-  });
+return gulp.src("./public/**/*")
+.pipe(deploy())
+});
